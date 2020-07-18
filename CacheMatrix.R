08464122e533c672868bd4089625fe547585b6a8
Alex Aponte <68233268+alalapre@users.noreply.github.com>
2020-07-18 11:10:58 -0400

    Update CacheMatrix.R

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -1,4 +1,9 @@
 makeCacheMatrix <- function(x = matrix()) {
+# Example input: Insert matrix e.g x<-matrix(rnorm(64),8,8)
+## To check cached values: 
+# xMat<-makeCacheMatrix(x)  # Run the function
+# parent.env(xMat$getenv())$m  # Check the cached mean
+# environment(xMat$getmean)  # refer to environment of "m"
 m<-NULL   
 evn <- environment()  
 y<-NULL 
@@ -19,7 +24,8 @@ getinverse = getinverse,
 getenv = getenv)
 
 }
-
+## The function "cacheSolve" returns the inverse of the matrix that is 
+# returned by makeCacheMatrix function, e.g. xMat$getmatrix()
 
 cacheSolve <- function(xMat= m(), ...) {
 	
@@ -36,5 +42,6 @@ cacheSolve <- function(xMat= m(), ...) {
     	xMat$setmatrix(y) 
     	m <- solve(y, ...) 
     	xMat$setinverse(m) 
-    	m     	}
-    	}
+    	m    # return the inverse
+	}
+}
